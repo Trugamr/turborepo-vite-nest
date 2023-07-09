fd29e00d7f4a9c4d6f75273d9c045fd2f19e3ff9
Trugamr <trugamr@live.in>
2023-07-09 12:16:37 +0530

    add prettier config

---
+++ packages/prettier-config-custom/index.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+}
+
+module.exports = config
